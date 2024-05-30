f8d7e9d486d711cd21b12c43740444894d3ad63a
KHJ <http://github.com/0515khj>
2024-05-30 15:35:12 +0900

    key

--- src/components/Login/Firebase.js
+++ src/components/Login/Firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDqpfWhilaFbIMCvM3XMPj8O1oyW5PeRqQ",
-    authDomain: "fbgogo-2bc40.firebaseapp.com",
-    projectId: "fbgogo-2bc40",
-    storageBucket: "fbgogo-2bc40.appspot.com",
-    messagingSenderId: "1048124884912",
-    appId: "1:1048124884912:web:921a05c18e83a11d697875",
-    measurementId: "G-YVQ662H1DT"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
   };
   
   const app = initializeApp(firebaseConfig);
